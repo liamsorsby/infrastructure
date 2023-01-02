3abe0c5246a59a065b3180b899f090b3b7e94632
Liam Sorsby <liam.sorsby@sky.com>
2023-01-02 21:31:45 +0000

    Fix AWS budget. Must be > 0

--- aws.tf
+++ aws.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "website" {
   name         = "monthly-website-budget"
   budget_type  = "COST"
-  limit_amount = "0.00"
+  limit_amount = "10.00"
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 }
